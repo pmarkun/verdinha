0b75ef6fc9160cbbb885a6142d9cceab36eaf9d0
Pedro Markun <pedro@markun.com.br>
2014-08-24 01:58:44 -0300

    mudancas no server para suportar 2010 e 2014

--- server/server.py
+++ server/server.py
@@ -8,6 +8,7 @@ app.config['MONGO_DBNAME'] = 'verdinha'
 #app.config["SECRET_KEY"] = "KeepThisS3cr3t"
 mongo = PyMongo(app)
 
+#tmp
 @app.route("/")
 def index():
 	return render_template('front.html')
@@ -15,12 +16,23 @@ def index():
 @app.route("/busca/<nome>")
 @app.route("/busca/<nome>/<ext>")
 def busca(nome, ext='html'):
-	resultado = mongo.db.doacoes.find_one({"nome": nome})
+	resultado = mongo.db.politicos.find_one({"nome": nome})
 	if resultado:
 		if ext == 'json':
 			return jsonify(resultado)
 		else:
-			resultado['doacoes'] = sorted(resultado['doacoes'].iteritems(), key=lambda x: x[1]['valor'], reverse=True)[0:5]
+			if not resultado['candidaturas'].has_key('2010'):
+				resultado['candidaturas']['2010'] = {
+					'total' : 0
+				}
+			else:
+				resultado['candidaturas']['2010']['doacoes'] = sorted(resultado['candidaturas']['2010']['doacoes'].iteritems(), key=lambda x: x[1]['valor'], reverse=True)[0:5]
+			if not resultado['candidaturas'].has_key('2014'):
+				resultado['candidaturas']['2014'] = {
+					'total' : 0
+				}
+			else:
+				resultado['candidaturas']['2014']['doacoes'] = sorted(resultado['candidaturas']['2014']['doacoes'].iteritems(), key=lambda x: x[1]['valor'], reverse=True)[0:5]
 			return render_template('popup.html', data=resultado)
 	else:
 		abort(404)
